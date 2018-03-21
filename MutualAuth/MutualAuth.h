3cb51566c54845e3d2d3066abb35a21e69d8758f
Erick Arroyo <iarroyo@belatrixsf.com>
2018-03-20 19:42:33 -0500

    Starting project

---
+++ MutualAuth/MutualAuth.h
@@ -0,0 +1,19 @@
+//
+//  MutualAuth.h
+//  MutualAuth
+//
+//  Created by hsierra on 3/20/18.
+//  Copyright © 2018 hsierra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MutualAuth.
+FOUNDATION_EXPORT double MutualAuthVersionNumber;
+
+//! Project version string for MutualAuth.
+FOUNDATION_EXPORT const unsigned char MutualAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MutualAuth/PublicHeader.h>
+
+
---
+++ MutualAuth/Response.swift
@@ -0,0 +1,13 @@
+//
+//  Response.swift
+//  MutualAuth
+//
+//  Created by hsierra on 3/20/18.
+//  Copyright © 2018 hsierra. All rights reserved.
+//
+
+import Foundation
+
+class Response {
+    
+}
